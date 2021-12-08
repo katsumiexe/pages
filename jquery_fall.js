48ea78cb2df0c9f7c7a4aff8dfbea2127e6f9491
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-12-08 10:06:56 +0900

    ar

---
+++ jquery_fall.js
@@ -0,0 +1,27 @@
+$(function(){ 
+	for(var T1=0; T1<30;S++){
+		var T2=T1 % 4;
+		Tag="<img id=\"f"+T1+"\" src=\"https://katsumiexe.github.io/img/fall/b"+T2+".png\" style=\"position:absolute;top:-20px;\">"
+	}
+
+	$('.main_fall').append(Tag):
+	timerId = setInterval(Fall,400);
+	function Fall(){
+		S +=1;
+		var N=S % 30;
+		var Rnd_st=Math.floor(Math.random() * 600);
+		var Rnd_ed=Math.floor(Math.random() * 600);
+		var Rnd_sp=Math.floor(Math.random() * 6000)+5999;
+		var Rnd_ro=Math.floor(Math.random() * 600)+400;
+		$('#f'+N).css({'top':'-30px','left':Rnd_st+'px'});
+		$('#f'+N).animate({'top':'110vh','left':Rnd_ed+'px'},Rnd_sp);
+		$({deg:0}).animate({deg:Rnd_ro}, {
+			duration:Rnd_sp,
+			progress:function() {
+				$('#f'+N).css({
+					'transform':'rotate(' + this.deg + 'deg)',
+				});
+			},
+		});
+	}
+});

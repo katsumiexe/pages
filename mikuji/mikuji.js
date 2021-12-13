572bf40e66d134c6a188f9b4ca594a4701515f29
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-12-13 17:19:43 +0900

    ar

--- mikuji/mikuji.js
+++ mikuji/mikuji.js
@@ -4,12 +4,12 @@ $(function(){
 
 	var Tag="";
 	Tag +="<div class=\"mikuji_box_0\">";
-	Tag +="<img src=\"https://github.io/pages/mikuji/img/mikuji.png\" class=\"mikuji_img_0\">";
-	Tag +="<img id=\"m0\" src=\"https://github.io/pages/mikuji/img/mikuji_1.png\" class=\"mikuji_img_1\">";
-	Tag +="<img id=\"m1\" src=\"https://github.io/pages/mikuji/img/mikuji_2.png\" class=\"mikuji_img_1\">";
-	Tag +="<img id=\"m2\" src=\"https://github.io/pages/mikuji/img/mikuji_3.png\" class=\"mikuji_img_1\">";
-	Tag +="<img id=\"m3\" src=\"https://github.io/pages/mikuji/img/mikuji_4.png\" class=\"mikuji_img_1\">";
-	Tag +="<img src=\"https://github.io/pages/mikuji/img/mikuji_t.png\" class=\"mikuji_img_2\">";
+	Tag +="<img src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji.png\" class=\"mikuji_img_0\">";
+	Tag +="<img id=\"m0\" src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji_1.png\" class=\"mikuji_img_1\">";
+	Tag +="<img id=\"m1\" src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji_2.png\" class=\"mikuji_img_1\">";
+	Tag +="<img id=\"m2\" src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji_3.png\" class=\"mikuji_img_1\">";
+	Tag +="<img id=\"m3\" src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji_4.png\" class=\"mikuji_img_1\">";
+	Tag +="<img src=\"https://katsumiexe.github.io/pages/mikuji/img/mikuji_t.png\" class=\"mikuji_img_2\">";
 	Tag +="</div>";
 	Tag +="<div class=\"mikuji_box_cover\"></div>";
 	$('.main_mikuji').append(Tag);

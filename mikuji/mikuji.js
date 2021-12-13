b2dae3e8636d83ffe09a03701c80801d8cffe07b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-12-14 08:31:38 +0900

    ar

--- mikuji/mikuji.js
+++ mikuji/mikuji.js
@@ -23,6 +23,7 @@ $(function(){
 console.log($('.main_mikuji').css("position"));
 
 	$('.mikuji_box_0').draggable({
+ 		containment: 'parent',
  		drag: function(e, ui) {
 			Tmp_deg += Tmp_deg_add;
 			if(Tmp_deg > 5){

7187b91d2f8a31732aff72aed29473487930915d
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-12-21 11:48:52 +0900

    ar

--- fall/fall.js
+++ fall/fall.js
@@ -2,7 +2,7 @@ $(function(){
 $('.main_fall').css({'width':'100vw','min-height':'90vh','overflow':'hidden'});
 	var S=0;
 	var Tag="";
-	var Ht=$('.main_fall').height()+20;
+	var Ht=$('.main_fall').height()+100;
 	for(var T1=0; T1<30;T1++){
 		var T2=T1 % 4;
 		Tag += '<img id=\"f'+T1+'\" src=\"https://katsumiexe.github.io/pages/fall/img/b'+T2+'.png\" class=\"fall\" style=\"position:absolute;top:-5vh;left:'+Math.floor(Math.random() * 60)+20+'vw;\">';

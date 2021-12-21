1581bda305b13318111255b01d092aa79bdc731f
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2021-12-21 11:53:53 +0900

    ar

--- fall/fall.js
+++ fall/fall.js
@@ -2,7 +2,7 @@ $(function(){
 $('.main_fall').css({'width':'100vw','min-height':'90vh','overflow':'hidden'});
 	var S=0;
 	var Tag="";
-	var Ht=$('.main_fall').height()+100;
+	var Ht=$('.main_fall').height()+50;
 	for(var T1=0; T1<30;T1++){
 		var T2=T1 % 4;
 		Tag += '<img id=\"f'+T1+'\" src=\"https://katsumiexe.github.io/pages/fall/img/b'+T2+'.png\" class=\"fall\" style=\"position:absolute;top:-5vh;left:'+Math.floor(Math.random() * 60)+20+'vw;\">';
@@ -24,25 +24,7 @@ $('.main_fall').css({'width':'100vw','min-height':'90vh','overflow':'hidden'});
 		var Rnd_sp=Math.floor(Math.random() * 12000)+3000;
 		var Rnd_ro=Math.floor(Math.random() * 600)+400;
 
-	//		$('#f'+N).css({'top':'-10vh','left':Rnd_st+'px'});
-	//		$('.fall').css({'top':'-10vh'});
-
-		$('#f'+N)
-			.animate({'top':'-5vh','left':Rnd_st+'vw'},0)
-			.animate({'top':Ht,'left':Rnd_ed+'vw'},Rnd_sp);
-
-	/*
-		$('.fall').animate({'top':'-5vh','left':Rnd_st+'vw'},1,
-			function(){
-				$('#f'+N).animate({'top':'110vh','left':Rnd_ed+'vw'},Rnd_sp)
-			}
-		);
-	/*
-
-		$('.fall').css({'top':'-5vh'});
-		$('#f'+N).animate({'top':'110vh','left':Rnd_ed+'vw'},Rnd_sp)
-	*/	
-
+		$('#f'+N).animate({'top':'-5vh','left':Rnd_st+'vw'},0).animate({'top':Ht,'left':Rnd_ed+'vw'},Rnd_sp).fadeOut(500).fadeIn(0);
 
 		$({deg:0}).animate({deg:Rnd_ro}, {
 			duration:Rnd_sp,
